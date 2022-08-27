da2c94c50ee757340fef4f5acedfcf43668f1319
ooliver1 <oliverwilkes2006@icloud.com>
2022-08-28 00:53:26 +0100

    refactor(events): use spawning for async event

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -2,16 +2,25 @@ mod message;
 mod shard;
 
 use crate::types::ThangResult;
+use log::error;
 use message::on_message;
 use shard::on_shard_connect;
-use std::sync::Arc;
+use std::{future::Future, sync::Arc};
 use twilight_gateway::Event;
 use twilight_http::Client;
 
-pub async fn handle_event(shard_id: u64, event: Event, http: Arc<Client>) -> ThangResult<()> {
-    return match event {
-        Event::MessageCreate(msg) => on_message(*msg, http).await,
+pub async fn handle_event(shard_id: u64, event: Event, http: Arc<Client>) {
+    match event {
         Event::ShardConnected(_) => on_shard_connect(shard_id),
-        _ => Ok(()),
+        Event::MessageCreate(msg) => async_wrapper(on_message(*msg, http), "message_create"),
+        _ => {}
     };
+}
+
+fn async_wrapper(todo: impl Future<Output = ThangResult<()>> + Send + 'static, name: &'static str) {
+    tokio::spawn(async move {
+        if let Err(e) = todo.await {
+            error!("Failed to execute {} handler: {}", name, e);
+        }
+    });
 }
--- src/events/shard.rs
+++ src/events/shard.rs
@@ -1,7 +1,5 @@
-use crate::types::ThangResult;
 use log::info;
 
-pub fn on_shard_connect(shard_id: u64) -> ThangResult<()> {
+pub fn on_shard_connect(shard_id: u64) {
     info!("Connected on shard {}", shard_id);
-    Ok(())
 }
