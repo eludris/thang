ff2456008dc0bd1ba7dd84f63e2c4b158f465e4c
ooliver1 <oliverwilkes2006@icloud.com>
2022-08-27 23:46:51 +0100

    feat: use twilight

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use futures::stream::StreamExt;
+use log::info;
+use std::{env, error::Error, sync::Arc};
+use twilight_gateway::{
+    cluster::{Cluster, ShardScheme},
+    Event, Intents,
+};
+use twilight_http::Client as HttpClient;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    dotenv().ok();
+    env_logger::init();
+
+    let token = env::var("TOKEN")?;
+
+    let scheme = ShardScheme::Range {
+        from: 0,
+        to: 0,
+        total: 1,
+    };
+
+    let intents = Intents::GUILD_MESSAGES | Intents::MESSAGE_CONTENT;
+
+    let (cluster, mut events) = Cluster::builder(token.clone(), intents)
+        .shard_scheme(scheme)
+        .build()
+        .await?;
+
+    let cluster = Arc::new(cluster);
+
+    let cluster_spawn = cluster.clone();
+
+    tokio::spawn(async move {
+        cluster_spawn.up().await;
+    });
+
+    // The http client is seperate from the gateway, so startup a new
+    // one, also use Arc such that it can be cloned to other threads.
+    let http = Arc::new(HttpClient::new(token));
+
+    while let Some((shard_id, event)) = events.next().await {
+        tokio::spawn(handle_event(shard_id, event, Arc::clone(&http)));
+    }
+
+    Ok(())
+}
+
+async fn handle_event(
+    shard_id: u64,
+    event: Event,
+    http: Arc<HttpClient>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match event {
+        Event::MessageCreate(msg) => {
+            if msg.content == "!ping" {
+                http.create_message(msg.channel_id)
+                    .content("Pong!")?
+                    .exec()
+                    .await?;
+            } else if msg.content == "!help" {
+                http.create_message(msg.channel_id)
+                    .content("L")?
+                    .exec()
+                    .await?;
+            }
+        }
+        Event::ShardConnected(_) => {
+            info!("Connected on shard {}", shard_id);
+        }
+        _ => {}
+    }
+
+    Ok(())
 }
