1bca97cae14cecb60945c7b959689989fecc0445
ooliver1 <oliverwilkes2006@icloud.com>
2022-08-28 19:35:45 +0100

    feat(types): add types for eludris msg and websocket

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,19 @@
 use std::error::Error;
 
+use futures::stream::{SplitSink, SplitStream};
+use serde::{Deserialize, Serialize};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{
+    tungstenite::protocol::Message as TungstenMessage, MaybeTlsStream, WebSocketStream,
+};
+use twilight_model::id::{
+    marker::{ChannelMarker, WebhookMarker},
+    Id,
+};
+
 pub type ThangResult<T> = Result<T, Box<dyn Error + Send + Sync>>;
+pub type WsWriter = SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, TungstenMessage>;
+pub type WsReader = SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>;
 
 pub struct Context {
     pub http: twilight_http::Client,
@@ -12,4 +25,10 @@ pub struct Context {
     pub eludris_rest_url: String,
     pub eludris_http_client: reqwest::Client,
     pub eludris_ws_writer: WsWriter,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Message {
+    pub author: String,
+    pub content: String,
 }
