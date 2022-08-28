c6fc2ae9b3d834414c62e0928c28a94abed4713b
ooliver1 <oliverwilkes2006@icloud.com>
2022-08-28 20:10:18 +0100

    refactor: dont panic its ok thang

--- src/eludris/mod.rs
+++ src/eludris/mod.rs
@@ -1,14 +1,14 @@
-use crate::types::{Context, Message, WsReader};
+use crate::types::{Context, Message, ThangResult, WsReader};
 use futures::StreamExt;
 use std::sync::Arc;
 
-pub async fn iterate_websocket(mut reader: WsReader, context: Arc<Context>) {
+pub async fn iterate_websocket(mut reader: WsReader, context: Arc<Context>) -> ThangResult<()> {
     while let Some(message) = reader.next().await {
         let data = message.unwrap().into_text().unwrap();
         let msg: Message = serde_json::from_str(&data).unwrap();
 
         if !msg.author.starts_with("Bridge-") {
-            context
+            if let Err(e) = context
                 .http
                 .execute_webhook(context.bridge_webhook_id, &context.bridge_webhook_token)
                 .content(&msg.content)
@@ -17,7 +17,11 @@ pub async fn iterate_websocket(mut reader: WsReader, context: Arc<Context>) {
                 .unwrap()
                 .exec()
                 .await
-                .unwrap();
+            {
+                return Err(Box::new(e));
+            }
         }
     }
+
+    Ok(())
 }
