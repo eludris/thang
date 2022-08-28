96c4b25714ff144dba9943189baa5df7e1c83b9a
ooliver1 <oliverwilkes2006@icloud.com>
2022-08-28 23:40:22 +0100

    fix: validate username and content before sending

--- src/eludris/mod.rs
+++ src/eludris/mod.rs
@@ -1,6 +1,8 @@
 use crate::types::{Context, Message, ThangResult, WsReader};
 use futures::StreamExt;
 use std::sync::Arc;
+use twilight_validate::message::MESSAGE_CONTENT_LENGTH_MAX;
+use twilight_validate::request::webhook_username as validate_webhook_username;
 
 pub async fn iterate_websocket(mut reader: WsReader, context: Arc<Context>) -> ThangResult<()> {
     while let Some(message) = reader.next().await {
@@ -8,18 +10,26 @@ pub async fn iterate_websocket(mut reader: WsReader, context: Arc<Context>) -> T
         let msg: Message = serde_json::from_str(&data).unwrap();
 
         if !msg.author.starts_with("Bridge-") {
-            if let Err(e) = context
+            let username = match validate_webhook_username(&msg.author) {
+                Ok(_) => &msg.author,
+                Err(_) => "Eludris Bridge",
+            };
+
+            let truncated = format!("{}...", &msg.content[..MESSAGE_CONTENT_LENGTH_MAX - 3]);
+            let content = match msg.content.chars().count() <= MESSAGE_CONTENT_LENGTH_MAX {
+                true => msg.content,
+                false => truncated,
+            };
+
+            context
                 .http
                 .execute_webhook(context.bridge_webhook_id, &context.bridge_webhook_token)
-                .content(&msg.content)
+                .content(&content)
                 .unwrap()
-                .username(&msg.author)
+                .username(username)
                 .unwrap()
                 .exec()
-                .await
-            {
-                return Err(Box::new(e));
-            }
+                .await?;
         }
     }
 
