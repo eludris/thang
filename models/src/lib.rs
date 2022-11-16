19f2910d38e25b766a2a18634ad49158bed39a92
Enoki <enokiun@gmail.com>
2022-11-16 21:04:19 +0300

    feat: update Event enum to include more events

--- models/src/lib.rs
+++ models/src/lib.rs
@@ -1,10 +1,27 @@
 use serde::{Deserialize, Serialize};
-use todel::models::Message as EludrisMessage;
-use twilight_model::channel::Message as DiscordMessage;
+use todel::models::Payload as EludrisEvent;
+use twilight_model::gateway::payload::incoming::*;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "platform")]
-pub enum Message {
-    Eludris(EludrisMessage),
-    Discord(DiscordMessage),
+pub enum Event {
+    Eludris(EludrisEvent),
+    Discord(DiscordEvent),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "t", content = "d")]
+pub enum DiscordEvent {
+    ChannelPinsUpdate(ChannelPinsUpdate),
+    ChannelUpdate(Box<ChannelUpdate>),
+    GuildEmojisUpdate(GuildEmojisUpdate),
+    GuildStickersUpdate(GuildStickersUpdate),
+    MessageCreate(Box<MessageCreate>),
+    MessageDelete(MessageDelete),
+    MessageDeleteBulk(MessageDeleteBulk),
+    MessageUpdate(Box<MessageUpdate>),
+    ReactionAdd(Box<ReactionAdd>),
+    ReactionRemove(Box<ReactionRemove>),
+    ReactionRemoveAll(ReactionRemoveAll),
+    ReactionRemoveEmoji(ReactionRemoveEmoji),
 }
